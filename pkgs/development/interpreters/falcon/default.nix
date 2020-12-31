36999b998442920e78ddb463d7cf0f7e3d07c0db
Ben Siraphob <bensiraphob@gmail.com>
2020-12-31 19:44:01 +0700

    falcon: 2013-09-19 -> unstable-2018-10-23

--- pkgs/development/interpreters/falcon/default.nix
+++ pkgs/development/interpreters/falcon/default.nix
@@ -1,18 +1,18 @@
-{ stdenv, fetchFromGitHub, cmake, pkgconfig, pcre, zlib, sqlite }:
+{ stdenv, fetchFromGitHub, cmake, pkg-config, pcre, zlib, sqlite }:
 
 stdenv.mkDerivation {
   pname = "falcon";
-  version = "2013-09-19";
+  version = "unstable-2018-10-23";
 
   src = fetchFromGitHub {
     owner = "falconpl";
     repo = "falcon";
-    rev = "095141903c4ebab928ce803055f9bda363215c37";
-    sha256 = "1x3gdcz1gqhi060ngqi0ghryf69v8bn50yrbzfad8bhblvhzzdlf";
+    rev = "637e2d5cd950a874496042993c02ab7d17c1b688";
+    sha256 = "iCyvvZJjXb1CR396EJ6GiP6d4e7iAc6QQlAOQoAfehg=";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ cmake pcre zlib sqlite ];
+  nativeBuildInputs = [ cmake pkg-config ];
+  buildInputs = [ pcre zlib sqlite ];
 
   meta = with stdenv.lib; {
     description = "Programming language with macros and syntax at once";
